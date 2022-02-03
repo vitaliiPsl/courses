79d55e1cfd5888cd4c8db46247abb2fdcd62ccd3
vitaliiPsl <vitalii.psl.main@gmail.com>
2022-02-03 22:20:53 +0200

    Create Course service and validation

---
+++ src/main/java/com/example/courses/service/CourseService.java
@@ -0,0 +1,156 @@
+package com.example.courses.service;
+
+import com.example.courses.persistence.CourseDAO;
+import com.example.courses.persistence.DAOFactory;
+import com.example.courses.persistence.entity.Course;
+import com.example.courses.utils.CourseValidation;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CourseService {
+    private final DAOFactory daoFactory;
+    private final CourseDAO courseDao;
+
+    public CourseService(){
+        daoFactory = DAOFactory.getDAOFactory(DAOFactory.FactoryType.POSTGRES);
+        courseDao = daoFactory.getCourseDao();
+    }
+
+    public long saveNewCourse(Course course) throws SQLException {
+        if(!CourseValidation.isCourseValid(course)){
+            throw new IllegalArgumentException("You have to provide valid data");
+        }
+
+        return saveCourse(course);
+    }
+
+    public long saveCourse(Course course) throws SQLException {
+        long courseId;
+        Connection connection = null;
+
+        try{
+            connection = daoFactory.getConnection();
+            courseId = courseDao.saveCourse(connection, course);
+            connection.commit();
+        } catch (SQLException e) {
+            DAOFactory.rollback(connection);
+            System.out.println(e.getMessage());
+            throw e;
+        } finally {
+            DAOFactory.closeResource(connection);
+        }
+
+        return courseId;
+    }
+
+    public void deleteCourse(long id) throws SQLException {
+        Connection connection = null;
+
+        try{
+            connection = daoFactory.getConnection();
+            courseDao.deleteCourseById(connection, id);
+            connection.commit();
+        } catch (SQLException e) {
+            DAOFactory.rollback(connection);
+            System.out.println(e.getMessage());
+            throw e;
+        } finally {
+            DAOFactory.closeResource(connection);
+        }
+    }
+
+    public void updateCourse(Course course) throws SQLException {
+        Connection connection = null;
+
+        try{
+            connection = daoFactory.getConnection();
+            courseDao.updateCourse(connection, course);
+            connection.commit();
+        } catch (SQLException e) {
+            DAOFactory.rollback(connection);
+            System.out.println(e.getMessage());
+            throw e;
+        } finally {
+            DAOFactory.closeResource(connection);
+        }
+    }
+    public Course getCourseById(long courseId){
+        Course course = null;
+        Connection connection = null;
+
+        try{
+            connection = daoFactory.getConnection();
+            course = courseDao.findCourse(connection, courseId);
+            connection.commit();
+        } catch (SQLException e) {
+            DAOFactory.rollback(connection);
+            System.out.println(e.getMessage());
+        } finally {
+            DAOFactory.closeResource(connection);
+        }
+
+        return course;
+    }
+
+    public List<Course> getCourses(List<Long> ids) throws SQLException {
+        List<Course> courseList = new ArrayList<>();
+        Connection connection = null;
+
+        try{
+            connection = daoFactory.getConnection();
+            for(long id: ids) {
+                courseList.add(courseDao.findCourse(connection, id));
+            }
+            connection.commit();
+        } catch (SQLException e) {
+            DAOFactory.rollback(connection);
+            System.out.println(e.getMessage());
+            throw e;
+        } finally {
+            DAOFactory.closeResource(connection);
+        }
+
+        return courseList;
+    }
+
+    public List<Course> getCoursesBySearchQuery(String query) throws SQLException {
+        List<Course> courseList = null;
+        Connection connection = null;
+
+        try{
+            connection = daoFactory.getConnection();
+            courseList = courseDao.findCoursesBySearchQuery(connection, query);
+            connection.commit();
+        } catch (SQLException e) {
+            DAOFactory.rollback(connection);
+            System.out.println(e.getMessage());
+            throw e;
+        } finally {
+            DAOFactory.closeResource(connection);
+        }
+
+        return courseList;
+    }
+
+    public List<Course> getCoursesByTeacherId(long teacherId) throws SQLException {
+        List<Course> courseList = null;
+        Connection connection = null;
+
+        try{
+            connection = daoFactory.getConnection();
+            courseList = courseDao.findCoursesByTeacherId(connection, teacherId);
+            connection.commit();
+        } catch (SQLException e) {
+            DAOFactory.rollback(connection);
+            System.out.println(e.getMessage());
+            throw e;
+        } finally {
+            DAOFactory.closeResource(connection);
+        }
+
+        return courseList;
+    }
+}
---
+++ src/main/java/com/example/courses/utils/CourseValidation.java
@@ -0,0 +1,39 @@
+package com.example.courses.utils;
+
+import com.example.courses.persistence.entity.Course;
+
+public class CourseValidation {
+    static final int MAX_TITLE_LENGTH = 100;
+    static final int MAX_SUBJECT_LENGTH = 100;
+    static final int MAX_DESCRIPTION_LENGTH = 512;
+
+    private CourseValidation(){}
+
+    public static boolean isCourseValid(Course course){
+        return course != null
+                && isTitleValid(course.getTitle())
+                && isSubjectValid(course.getSubject())
+                && isDescriptionValid(course.getDescription());
+    }
+
+    public static boolean isTitleValid(String title) {
+        return title != null
+                && !title.trim().isEmpty()
+                && title.length() <= MAX_TITLE_LENGTH;
+    }
+
+    public static boolean isSubjectValid(String subject) {
+        return subject != null
+                && !subject.trim().isEmpty()
+                && subject.length() <= MAX_SUBJECT_LENGTH;
+    }
+
+    public static boolean isDescriptionValid(String description) {
+        if(description != null){
+            return description.length() < MAX_DESCRIPTION_LENGTH;
+        }
+
+        return true;
+    }
+
+}
