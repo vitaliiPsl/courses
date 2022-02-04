bb9d2438a3f773dd3b0014efbef36bb5644bdb2b
vitaliiPsl <vitalii.psl.main@gmail.com>
2022-02-04 18:58:06 +0200

    Move common logic for NewCourse and EditCourse into class CourseUtils

---
+++ src/main/java/com/example/courses/utils/CourseUtils.java
@@ -0,0 +1,34 @@
+package com.example.courses.utils;
+
+import com.example.courses.persistence.entity.Course;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
+
+public class CourseUtils {
+    private CourseUtils() {
+    }
+
+    public static Course buildCourse(HttpServletRequest request) {
+        String name = request.getParameter("course_title");
+        String subject = request.getParameter("course_subject");
+        String description = request.getParameter("course_description");
+        long teacherId = Long.parseLong(request.getParameter("teacher_id"));
+        long languageId = Long.parseLong(request.getParameter("language_id"));
+        int maxScore = Integer.parseInt(request.getParameter("max_score"));
+        LocalDateTime startDate = LocalDateTime.parse(request.getParameter("start_date"));
+        LocalDateTime endDate = LocalDateTime.parse(request.getParameter("end_date"));
+
+        Course course = new Course();
+        course.setTitle(name);
+        course.setSubject(subject);
+        course.setDescription(description);
+        course.setMaxScore(maxScore);
+        course.setStartDate(startDate);
+        course.setEndDate(endDate);
+        course.setTeacherId(teacherId);
+        course.setLanguageId(languageId);
+
+        return course;
+    }
+}
