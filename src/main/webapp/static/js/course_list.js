742f5564dfb198d5a9a2b1b894a4b87ea09b7919
vitaliiPsl <vitalii.psl.main@gmail.com>
2022-02-08 20:31:13 +0200

    Implement course sorting. js

--- src/main/webapp/static/js/course_list.js
+++ src/main/webapp/static/js/course_list.js
@@ -1,5 +1,33 @@
 const filters = document.querySelectorAll(".filter");
 
+const sortingOptions = document.querySelectorAll(".sort-option");
+const orderOptions = document.querySelectorAll(".order-option");
+
+addSortingClickListeners(sortingOptions);
+addSortingClickListeners(orderOptions);
+
+function addSortingClickListeners(options) {
+    options.forEach(option => {
+            option.addEventListener("click", () => {
+                unselectSortingOptions(options);
+
+                option.classList.add("selected")
+                let checkbox = option.querySelector("input");
+                checkbox.toggleAttribute("checked");
+            });
+        });
+}
+
+function unselectSortingOptions(options) {
+    options.forEach(
+            option => {
+                option.classList.remove("selected");
+                let checkbox = option.querySelector("input");
+                checkbox.removeAttribute("checked");
+            }
+        );
+}
+
 filters.forEach(filter => {
     let filterControl = filter.querySelector(".filter-control");
 
@@ -8,10 +36,17 @@ filters.forEach(filter => {
 
         let arrow = filterControl.querySelector(".arrow");
         arrow.classList.toggle("up");
-    })
+    });
 });
 
-function toggleFilterMenu(filter){
+function toggleFilterMenu(filter) {
     let filterMenu = filter.querySelector(".filter-menu");
-    filterMenu.classList.toggle("hidden");
+    if (filterMenu != null) {
+        filterMenu.classList.toggle("hidden");
+    }
+
+    let sortMenu = filter.querySelector(".sort-menu");
+    if (sortMenu != null) {
+        sortMenu.classList.toggle("hidden");
+    }
 }
