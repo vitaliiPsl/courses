0c802617f8a07cb717e9cfe675049d3120299d27
vitaliiPsl <vitalii.psl.main@gmail.com>
2022-02-18 18:05:56 +0200

    Add encoding filter

---
+++ src/main/java/com/example/courses/servlet/filter/RequestEncodingFilter.java
@@ -0,0 +1,13 @@
+package com.example.courses.servlet.filter;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class RequestEncodingFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        servletRequest.setCharacterEncoding("UTF-8");
+        servletResponse.setCharacterEncoding("UTF-8");
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+}
